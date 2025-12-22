31553c80d7ba22084e52fa2154b1874778d9fb70
Bidhya Ghimire <bidhyaghimire55@gmail.com>
2025-12-22 19:53:21 +0545

    init Staff model

---
+++ src/model/Staff.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.sql.Date;
+
+/**
+ *
+ * @author Bidhya
+ */
+public class Staff {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String position; 
+    private double salary;
+    private Date hireDate;
+    private Date birthDate;
+    private String gender;
+    private String status; 
+    private int userId; 
+}
